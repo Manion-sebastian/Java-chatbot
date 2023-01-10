faed301096650c425dcf1b82e272898d2d76210e
brandon-w1205 <bwu1205@gmail.com>
2023-01-09 17:23:12 -0800

    springboot init

---
+++ src/main/java/com/javaproject/javachatbot/JavaChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.javachatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChatbotApplication.class, args);
+	}
+
+}
